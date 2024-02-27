267ead84b625cd1983b4c03938eea4acedd10631
Aleksandr <PITON369@yandex.ru>
2024-02-27 13:26:51 +0300

    Feedback (Loading, Validation, Error Messages). Completed eighth video.

--- Reservoom/ViewModels/MakeReservationViewModel.cs
+++ Reservoom/ViewModels/MakeReservationViewModel.cs
@@ -1,9 +1,12 @@
-﻿using Reservoom.Commands;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Reservoom.Commands;
 using Reservoom.Models;
 using Reservoom.Services;
 using Reservoom.Stores;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +14,7 @@ using System.Windows.Input;
 
 namespace Reservoom.ViewModels
 {
-    public class MakeReservationViewModel : ViewModelBase
+    public class MakeReservationViewModel : ViewModelBase, INotifyDataErrorInfo
     {
         private string _userName;
         public string UserName
@@ -66,6 +69,13 @@ namespace Reservoom.ViewModels
             { 
                 _startdate = value; 
                 OnPropertyChanged(nameof(StartDate));
+
+                ClearErrors(nameof(EndDate));
+
+                if (EndDate < StartDate)
+                {
+                    AddError("The start date cannot be after the end date.", nameof(StartDate));
+                }
             }
         }
 
@@ -80,16 +90,60 @@ namespace Reservoom.ViewModels
             {
                 _endDate = value;
                 OnPropertyChanged(nameof(EndDate));
+
+                ClearErrors(nameof(StartDate));
+                ClearErrors(nameof(EndDate));
+
+                if (EndDate < StartDate)
+                {
+                    AddError("The end date cannot be before the start date.", nameof(EndDate));
+                }
             }
+        }
+
+        private void AddError(string errorMessage, string propertyName)
+        {
+            if (!_propertyNameToErrorsDictionary.ContainsKey(propertyName))
+            {
+                _propertyNameToErrorsDictionary.Add(propertyName, new List<string>());
+            }
+
+            _propertyNameToErrorsDictionary[propertyName].Add(errorMessage);
+
+            OnErrorsChanged(propertyName);
         }
 
         public ICommand SubmitCommand { get; }
         public ICommand CancelCommand { get; }
 
+        private readonly Dictionary<string, List<string>> _propertyNameToErrorsDictionary;
+
+        public bool HasErrors => _propertyNameToErrorsDictionary.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged; 
+
         public MakeReservationViewModel(HotelStore hotelStore, NavigationService reservationViewNavigationService)
         {
             SubmitCommand = new MakeReservationCommand(this, hotelStore, reservationViewNavigationService);
             CancelCommand = new NavigateCommand(reservationViewNavigationService);
+
+            _propertyNameToErrorsDictionary = new Dictionary<string, List<string>>();
+        }
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return _propertyNameToErrorsDictionary.GetValueOrDefault(propertyName, new List<string>());
+        }
+
+        private void ClearErrors(string propertyName)
+        {
+            _propertyNameToErrorsDictionary.Remove(nameof(propertyName));
+            OnErrorsChanged(propertyName);
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
         }
     }
 }
